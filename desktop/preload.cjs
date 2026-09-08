2539dedabd1427cbf96c860da274aa1688c29e5a
Jay-Codes4 <85906552+Jay-Codes4@users.noreply.github.com>
2026-09-08 02:20:05 +0100

    Expose desktop companion controls to renderer

--- desktop/preload.cjs
+++ desktop/preload.cjs
@@ -3,5 +3,6 @@ const { contextBridge, ipcRenderer } = require("electron");
 contextBridge.exposeInMainWorld("damiDesktop", {
   getDock: () => ipcRenderer.invoke("dami:get-dock"),
   setDock: (dock) => ipcRenderer.invoke("dami:set-dock", dock),
+  setLaunchAtStartup: (enabled) => ipcRenderer.invoke("dami:set-launch-at-startup", enabled),
   isDesktop: true,
 });
