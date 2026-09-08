0b2d8f3b6189d6ed2e510a9dd1fbf10250279e1b
Jay-Codes4 <85906552+Jay-Codes4@users.noreply.github.com>
2026-09-08 03:12:09 +0100

    Expose desktop companion window controls

--- desktop/preload.cjs
+++ desktop/preload.cjs
@@ -4,5 +4,7 @@ contextBridge.exposeInMainWorld("damiDesktop", {
   getDock: () => ipcRenderer.invoke("dami:get-dock"),
   setDock: (dock) => ipcRenderer.invoke("dami:set-dock", dock),
   setLaunchAtStartup: (enabled) => ipcRenderer.invoke("dami:set-launch-at-startup", enabled),
+  show: () => ipcRenderer.invoke("dami:show"),
+  hide: () => ipcRenderer.invoke("dami:hide"),
   isDesktop: true,
 });
